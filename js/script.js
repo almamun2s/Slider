d3952ff4e4a16cf260ff41efc5128ba2741fcd8a
Abdullah Almamun <mamun2social@gmail.com>
2024-02-07 21:43:35 +0600

    finished

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
 const slides = document.querySelectorAll('.sr-slider-item');
+const prev  = document.querySelector('.sr-prev-btn');
+const frwd  = document.querySelector('.sr-frwd-btn');
 
-console.log(slides);
+
+slides.forEach(function(slide, index){
+    slide.style.left = `${index * 100}%`;
+});
+
+
+let counter = 0;
+frwd.addEventListener('click', function(){
+    counter++;
+    carousel();
+});
+prev.addEventListener('click', function(){
+    counter--;
+    carousel();
+});
+
+function carousel() {
+    if (counter == slides.length) {
+        counter = 0;
+    }
+    if (counter < 0) {
+        counter = slides.length -1;
+    }
+    slides.forEach(function(slide) {
+        slide.style.transform = `translateX(-${counter * 100}%)`;
+    });
+    
+}
